35689d847ca9291628ca217997b5da866c7a0cd8
TegarAdityaS <mohammadtegaradityasaputra@gmail.com>
2023-11-16 20:29:52 +0700

    Pengembangan Fitur Produk

--- produk/indexProduk.php
+++ produk/indexProduk.php
@@ -175,8 +175,14 @@
               <!-- FORM TAMBAH PRODUK END -->
 
               <h5 class="pb-1 mb-4"></h5>
-              <div class="row mb-5">
-              <?php foreach ($sql as $dataproduk): ?>
+              <!-- <div class="row mb-5"> -->
+              <?php 
+              $counter = 0; // Initialize a counter
+              foreach ($sql as $dataproduk): 
+                if ($counter % 2 == 0) {
+                  // Start a new row for every two products
+                  echo '<div class="row mb-3">';
+                } ?>
                 <div class="col-md">
                   <div class="card mb-3">
                     <div class="row g-0">
@@ -198,27 +204,16 @@
                     </div>
                   </div>
                 </div>
-                <?php endforeach ?>
-                <!-- <div class="col-md">
-                  <div class="card mb-3">
-                    <div class="row g-0">
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">
-                            This is a wider card with supporting text below as a natural lead-in to additional content.
-                            This content is a little bit longer.
-                          </p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <img class="card-img card-img-right" src="../assets/img/elements/17.jpg" alt="Card image" />
-                      </div>
-                    </div>
-                  </div>
-                </div> -->
-              </div>
+                <?php $counter++;
+                    if ($counter % 2 == 0) {
+                      // Close the row after every two products
+                      echo '</div>';
+                    }
+                  endforeach; 
+                  // Close the row if there are an odd number of products
+                  if ($counter % 2 != 0) {
+                    echo '</div>';
+                } ?>
 
 
 
